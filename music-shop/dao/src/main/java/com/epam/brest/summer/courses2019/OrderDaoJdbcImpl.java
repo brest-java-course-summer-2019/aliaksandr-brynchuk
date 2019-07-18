8d64458de9df63a2da7006e7b5333dfad90e9b94
Aliaksandr Brynchuk <alan23westwood@gmail.com>
2019-07-18 13:22:17 +0300

    update pom files

--- music-shop/dao/src/main/java/com/epam/brest/summer/courses2019/OrderDaoJdbcImpl.java
+++ music-shop/dao/src/main/java/com/epam/brest/summer/courses2019/OrderDaoJdbcImpl.java
@@ -1,11 +1,25 @@
 package com.epam.brest.summer.courses2019;
 
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Component;
+
 import java.util.List;
 
+@Component
 public class OrderDaoJdbcImpl implements OrderDao {
+
+    private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+
+    private static final String SELECT_ALL = "1";
+
+    public OrderDaoJdbcImpl(NamedParameterJdbcTemplate namedParameterJdbcTemplate) {
+        this.namedParameterJdbcTemplate = namedParameterJdbcTemplate;
+    }
+
 
     @Override
     public Order addOrder(Order order) {
+
         return null;
     }
 
