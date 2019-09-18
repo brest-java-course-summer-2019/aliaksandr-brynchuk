a10249d95e455d5dc442339d14961419ece6dc47
Aliaksandr Brynchuk <alan23westwood@gmail.com>
2019-09-19 01:55:00 +0300

    add dao for orderDTO

---
+++ music-shop/dao/src/main/java/com/epam/brest/summer/courses2019/OrderDTODaoJdbcImpl.java
@@ -0,0 +1,26 @@
+package com.epam.brest.summer.courses2019;
+
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+
+import java.util.List;
+
+
+public class OrderDTODaoJdbcImpl implements OrderDTODao {
+    private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+
+    private static final String SELECT_ALL_WITH_COST = "select o.order_id, o.order_number, o.order_date, sum(i.item_price) as orderCost" +
+            "from orders o" +
+            "left join items_order io on o.order_id = io.order_id" +
+            "inner join items i on i.item_id = io.item_id";
+
+    public OrderDTODaoJdbcImpl(NamedParameterJdbcTemplate namedParameterJdbcTemplate){
+        this.namedParameterJdbcTemplate = namedParameterJdbcTemplate;
+    }
+
+    @Override
+    public List<OrderDTO> findAllOrderDTOs() {
+        List<OrderDTO>orders = namedParameterJdbcTemplate.query(SELECT_ALL_WITH_COST, BeanPropertyRowMapper.newInstance(OrderDTO.class));
+        return orders;
+    }
+}
