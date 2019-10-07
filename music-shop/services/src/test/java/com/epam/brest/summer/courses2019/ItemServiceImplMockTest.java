7c75ae85260346bcc6f05178366dc3f718ebbc49
Aliaksandr Brynchuk <alan23westwood@gmail.com>
2019-10-08 01:46:51 +0300

    add mock test for order service

--- music-shop/services/src/test/java/com/epam/brest/summer/courses2019/ItemServiceImplMockTest.java
+++ music-shop/services/src/test/java/com/epam/brest/summer/courses2019/ItemServiceImplMockTest.java
@@ -14,7 +14,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.MockitoAnnotations.initMocks;
 
-public class ItemServiceImplMockTest {
+class ItemServiceImplMockTest {
 
     @Mock
     private ItemDao itemDao;
--- music-shop/services/src/test/java/com/epam/brest/summer/courses2019/OrderServiceImplMockTest.java
+++ music-shop/services/src/test/java/com/epam/brest/summer/courses2019/OrderServiceImplMockTest.java
@@ -5,11 +5,14 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.*;
 
+import java.time.LocalDate;
 import java.util.Collections;
+import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.MockitoAnnotations.initMocks;
 
-public class OrderServiceImplMockTest {
+class OrderServiceImplMockTest {
     @Mock
     OrderDao orderDao;
 
@@ -34,7 +37,57 @@ public class OrderServiceImplMockTest {
     @Test
     void findAllDTOs(){
         Mockito.when(orderDTODao.findAllOrderDTOs()).thenReturn(Collections.singletonList(createDto()));
+        List<OrderDTO> orders = orderService.findAllOrderDTOs();
+        assertNotNull(orders);
+        assertEquals(1, orders.size());
+    }
 
+    @Test
+    void findOrderById(){
+        int id = 1;
+        Mockito.when(orderDao.findOrderById(id)).thenReturn(create());
+
+        Order order = orderService.findOrderById(id);
+
+        assertTrue(order.getOrderId().equals(1));
+        Mockito.verify(orderDao).findOrderById(id);
+    }
+
+    @Test
+    void findOrdersByDates(){
+        Mockito.when(orderDTODao.findOrdersByDates(LocalDate.now(), LocalDate.now()))
+                .thenReturn(Collections.singletonList(createDto()));
+
+        List<OrderDTO> orders = orderService.findOrdersByDates(LocalDate.now(), LocalDate.now());
+        assertEquals(1, orders.size());
+
+        Mockito.verify(orderDTODao).findOrdersByDates(LocalDate.now(), LocalDate.now());
+    }
+
+    @Test
+    void addOrder(){
+//        Order order = create();
+//        Mockito.when(orderDao.addOrder(order), itemDao.)
+    }
+
+    @Test
+    void updateOrder(){
+    }
+
+    @Test
+    void deleteOrder(){
+        int id = 1;
+
+        orderService.deleteOrder(id);
+
+        Mockito.verify(orderDao).deleteOrder(id);
+
+    }
+
+    private static Order create(){
+        Order order = new Order();
+        order.setOrderId(1);
+        return order;
     }
 
     private static OrderDTO createDto(){
