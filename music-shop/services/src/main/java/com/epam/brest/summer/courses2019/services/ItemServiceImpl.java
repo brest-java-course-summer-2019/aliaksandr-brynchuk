044e60bf6991f902600452db949717b1ac33830b
Aliaksandr Brynchuk <alan23westwood@gmail.com>
2019-10-23 03:19:43 +0300

    add docs for item service impl

--- music-shop/services/src/main/java/com/epam/brest/summer/courses2019/services/ItemServiceImpl.java
+++ music-shop/services/src/main/java/com/epam/brest/summer/courses2019/services/ItemServiceImpl.java
@@ -11,38 +11,70 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
+/**
+ * Item Service implementation
+ */
 @Service
 @Transactional
 public class ItemServiceImpl implements ItemService {
 
+    /**
+     * Item DAO field
+     */
     private ItemDao dao;
 
+    /**
+     * Constructor, injection item dao bean
+     * @param dao Item DAO
+     */
     @Autowired
     public ItemServiceImpl(ItemDao dao){
          this.dao = dao;
     }
 
-
+    /**
+     * Logger
+     */
     private static final Logger LOGGER = LoggerFactory.getLogger(ItemServiceImpl.class);
 
+    /**
+     * Add item
+     *
+     * @param item Item
+     */
     @Override
     public void addItem(Item item) {
         LOGGER.debug("Add item: {}", item);
         dao.addItem(item);
     }
 
+    /**
+     * Update item
+     *
+     * @param item Item
+     */
     @Override
     public void updateItem(Item item) {
         LOGGER.debug("Update item: {}", item);
         dao.updateItem(item);
     }
 
+    /**
+     * Delete item
+     *
+     * @param itemId Item Id
+     */
     @Override
     public void deleteItem(Integer itemId) {
         LOGGER.debug("Delete item {}", itemId);
         dao.deleteItem(itemId);
     }
 
+    /**
+     * Delete item
+     *
+     * @return
+     */
     @Override
     public List<Item> findAllItems() {
         LOGGER.debug("ItemServiceImpl Find all items DAO - {}", dao);
@@ -50,6 +82,12 @@ public class ItemServiceImpl implements ItemService {
         return dao.findAllItems();
     }
 
+    /**
+     * Find item by id
+     *
+     * @param itemId Item Id
+     * @return Item
+     */
     @Override
     public Item findItemById(Integer itemId) {
         LOGGER.debug("Find item by id {}", itemId);
