8437ee9750a783cdbdc27cbc44f6d1745a4f9e2d
Aliaksandr Brynchuk <alan23westwood@gmail.com>
2019-10-23 03:53:56 +0300

    add docs for order rest controller

--- music-shop/rest-app/src/main/java/com/epam/brest/summer/courses2019/rest_app/OrderRestController.java
+++ music-shop/rest-app/src/main/java/com/epam/brest/summer/courses2019/rest_app/OrderRestController.java
@@ -12,19 +12,37 @@ import org.springframework.web.bind.annotation.*;
 import java.time.LocalDate;
 import java.util.List;
 
+/**
+ * Item REST controller
+ */
 @RestController
 @RequestMapping("inner/order")
 public class OrderRestController {
+
+    /**
+     * Logger
+     */
     private static final Logger LOGGER = LoggerFactory.getLogger(OrderRestController.class);
 
+    /**
+     * Order service bean
+     */
     private OrderService orderService;
 
+    /**
+     * Constructor, injection order service bean
+     * @param orderService Item Service
+     */
     @Autowired
     public OrderRestController(OrderService orderService) {
         this.orderService = orderService;
     }
 
-
+    /**
+     * Find all orderDTOs
+     *
+     * @return OrderDTOs List
+     */
     @GetMapping(value = "/orders")
     public List<OrderDTO> findAllOrderDTOs(){
         LOGGER.debug("OrderRestController: findAllOrderDTOs");
@@ -32,6 +50,12 @@ public class OrderRestController {
         return orderService.findAllOrderDTOs();
     }
 
+    /**
+     * Find order by ID
+     *
+     * @param id Order ID
+     * @return Order
+     */
     @GetMapping(value = "/{id}")
     @ResponseStatus(value = HttpStatus.OK)
     public Order findOrderById(@PathVariable Integer id){
@@ -40,6 +64,11 @@ public class OrderRestController {
         return orderService.findOrderById(id);
     }
 
+    /**
+     * Add order
+     *
+     * @param order Order
+     */
     @PostMapping
     @ResponseStatus(value = HttpStatus.CREATED)
     public void addOrder(@RequestBody Order order){
@@ -48,6 +77,11 @@ public class OrderRestController {
         orderService.addOrder(order);
     }
 
+    /**
+     * Update order
+     *
+     * @param order Order
+     */
     @PutMapping
     @ResponseStatus(value = HttpStatus.ACCEPTED)
     public void updateOrder(@RequestBody Order order){
@@ -56,6 +90,11 @@ public class OrderRestController {
         orderService.updateOrder(order);
     }
 
+    /**
+     * Delete order
+     *
+     * @param id Order ID
+     */
     @DeleteMapping(value = "/{id}/delete")
     public void deleteOrder(@PathVariable Integer id){
         LOGGER.debug("OrderRestController: deleteOrder({id})", id);
@@ -63,6 +102,13 @@ public class OrderRestController {
         orderService.deleteOrder(id);
     }
 
+    /**
+     * Find orderDTOs by dates
+     *
+     * @param dateFrom Date from
+     * @param dateTo Date to
+     * @return OrderDTOs List
+     */
     @GetMapping(value = "/orders/{from}/{to}")
     public List<OrderDTO> findOrdersByDates(@PathVariable("from") String dateFrom, @PathVariable("to") String dateTo) {
         LOGGER.debug("OrderRestController: findOrdersByDates({} - {})", dateFrom, dateTo);
