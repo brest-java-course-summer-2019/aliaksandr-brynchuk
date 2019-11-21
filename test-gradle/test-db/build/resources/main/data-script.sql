fdbd64e55aaf05b8fc3b87b7fe062c87eb1ad443
Aliaksandr Brynchuk <Aliaksandr_Brynchuk@epam.com>
2019-11-21 11:24:46 +0300

    delete trash

--- test-gradle/test-db/build/resources/main/data-script.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO films (name, genre, director) VALUES('aaaa', 'aaa', 'aa');
-INSERT INTO films (name, genre, director) VALUES('bbbb', 'bbb', 'bb');
-INSERT INTO films (name, genre, director) VALUES('cccc', 'ccc', 'cc');
-INSERT INTO films (name, genre, director) VALUES('dddd', 'ddd', 'dd');
