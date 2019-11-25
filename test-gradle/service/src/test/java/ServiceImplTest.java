677fe558ed8b43fb13cbf366f16e50bdb25775db
Aliaksandr Brynchuk <Aliaksandr_Brynchuk@epam.com>
2019-11-25 12:14:32 +0300

    update

--- test-gradle/service/src/test/java/ServiceImplTest.java
+++ test-gradle/service/src/test/java/ServiceImplTest.java
@@ -1,25 +1,25 @@
-import com.example.Film;
-import com.example.FilmService;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-
-@ExtendWith(SpringExtension.class)
-@ContextConfiguration(locations = {"classpath*:test-db.xml", "classpath*:test-dao.xml"})
-
-class ServiceImplTest {
-
-    @Autowired
-    private FilmService filmService;
-
-    @Test
-    void findById(){
-        int id = 1;
-        Film film = filmService.findFilmById(id);
-
-        System.out.println(film);
-    }
-
-}
+//import com.example.Film;
+//import com.example.FilmService;
+//import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.extension.ExtendWith;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.test.context.ContextConfiguration;
+//import org.springframework.test.context.junit.jupiter.SpringExtension;
+//
+//@ExtendWith(SpringExtension.class)
+//@ContextConfiguration(locations = {"classpath*:test-db.xml", "classpath*:test-dao.xml"})
+//
+//class ServiceImplTest {
+//
+//    @Autowired
+//    private FilmService filmService;
+//
+//    @Test
+//    void findById(){
+//        int id = 1;
+//        Film film = filmService.findFilmById(id);
+//
+//        System.out.println(film);
+//    }
+//
+//}
