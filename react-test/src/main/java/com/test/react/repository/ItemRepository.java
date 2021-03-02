ae750bb8302454041cd9247aa3f6dec20df08ae1
Aliaksandr Brynchuk <45766697+westwood23@users.noreply.github.com>
2021-03-02 20:32:31 +0300

    Delete react-test/src/main/java/com/test directory

--- react-test/src/main/java/com/test/react/repository/ItemRepository.java
+++
@@ -1,7 +0,0 @@
-package com.test.repository;
-
-import com.test.react.model.Item;
-import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-
-public interface ItemRepository extends ReactiveMongoRepository<Item, Integer> {
-}
